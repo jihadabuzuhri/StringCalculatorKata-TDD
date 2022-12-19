68c2b866312558b7cb8ef74685c2891af5aff8ab
jihadabuzuhri <jihadabuzuhri@gmail.com>
2022-12-19 15:21:43 +0200

    Step 1: the simplest thing

---
+++ StringCalculatorKata/StringCalculatorKata.cs
@@ -0,0 +1,14 @@
+﻿namespace StringCalculatorKataSolution
+{
+    public class StringCalculatorKata
+    {
+        public static int Add(string numbers)
+        {   if(String.IsNullOrEmpty(numbers) ) return 0;
+
+            var Splitednumbers = numbers.Split( ',' );
+
+            return Splitednumbers.Select(num => int.Parse(num)).Sum();
+
+        }
+    }
+}
---
+++ StringCalculatorKataTest/AddTest.cs
@@ -0,0 +1,39 @@
+using Xunit;
+
+namespace StringCalculatorKataSolution
+{
+    public class AddTest
+    {
+
+        [Fact]
+        public void EmptyStringTest()
+        {
+            String InputString = "";
+            
+            int result = StringCalculatorKata.Add(InputString);
+            
+            Assert.Equal(0, result);
+        }
+
+        [Theory]
+        [InlineData("1",1)]
+        [InlineData("2",2)]
+        public void ReturnsNumberWithOneNumber(string numbers, int expectedResult)
+        {
+            int result = StringCalculatorKata.Add(numbers);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("1,2", 3)]
+        [InlineData("2,4", 6)]
+        public void ReturnsNumberWithTwoCommaSeparatedNumber(string numbers, int expectedResult)
+        {
+            int result = StringCalculatorKata.Add(numbers);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+    }
+}
